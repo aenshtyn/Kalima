b09130359fd1038cf645c8d1407a97aa727445ec
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-09-22 15:02:56 +0300

    trial for heroku deployment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { BlogsComponent } from './blogs/blogs.component';
 import { ContactComponent } from './contact/contact.component';
+import { LandingComponent } from './landing/landing.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
 
   { path: 'about', component: AboutComponent},
-  { path: 'blogs', component: BlogsComponent},
+  { path: 'blog', component: BlogsComponent},
   { path: 'contact', component: ContactComponent},
-
-
-
-
+  { path: 'home', component: LandingComponent},
+  { path: 'products', component: ProductsComponent},
 ];
 
 @NgModule({
